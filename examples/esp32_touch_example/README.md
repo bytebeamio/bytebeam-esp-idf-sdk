b4793837f2fac49ded7729fda0e8392a305d2b2a
mayankbytebeam <mayanknarang@bytebeam.io>
2023-02-17 13:39:39 +0530

    temp humid example refactored

--- examples/esp32_touch_example/README.md
+++
@@ -1,5 +0,0 @@
-# Bytebeam ESP32 Touch Example
-ESP32 comes with capacitive touch GPIOs which can sense human touch. We will make use of these touch GPIO to send touch values to bytebeam cloud 
-## Hardware specification
-1. ESP32 Dev Board
-
--- examples/temp_humid/main/app_main.c
+++ examples/temp_humid/main/app_main.c
@@ -44,23 +44,50 @@
 
 #include "sht31.h"
 
-static int config_publish_period = 1000;
-float temperature = 0.0;
-float humidity = 90;
+// this macro is used to specify the delay for 1 sec.
+#define APP_DELAY_ONE_SEC 1000u
 
-char stream_name[15] = "sht_stream";
+static int config_publish_period = APP_DELAY_ONE_SEC;
 
-bytebeam_client_t bytebeam_client;
+static float temperature = 0.0;
+static float humidity = 90;
 
-static const char *TAG = "BYTEBEAM_DEMO_EXAMPLE";
+static char sht_stream[] = "sht_stream";
 
-static void getSHTValues(){
-     if (sht31_read_temp_humi(&temperature, &humidity))
-            printf("SHT3x Sensor: %.2f °C, %.2f %%\n",temperature, humidity);
+static bytebeam_client_t bytebeam_client;
+
+static const char *TAG = "BYTEBEAM_TEMP_HUMID_EXAMPLE";
+
+static void get_sht_values(void)
+{
+    int ret_val;
+
+    ret_val = sht31_read_temp_humi(&temperature, &humidity);
+    
+    if(ret_val != 0)
+    {
+        ESP_LOGE(TAG, "Failed to read sht values.");
+    }
+    
+    ESP_LOGI(TAG, "SHT3x Sensor : %.2f °C, %.2f %% \n",temperature, humidity);
 }
 
-static int publish_device_shadow(bytebeam_client_t *bytebeam_client)
+static void sht_init(void)
 {
+    int ret_val;
+
+    ret_val = sht31_init();
+
+    if(ret_val != 0)
+    {
+        ESP_LOGE(TAG, "Failed to initialized sht.");
+    }
+}
+
+static int publish_sht_values(bytebeam_client_t *bytebeam_client)
+{
+    struct timeval te;
+    long long milliseconds = 0;
     static uint64_t sequence = 0;
 
     cJSON *device_shadow_json_list = NULL;
@@ -69,33 +96,35 @@ static int publish_device_shadow(bytebeam_client_t *bytebeam_client)
     cJSON *timestamp_json = NULL;
     cJSON *device_status_json = NULL;
     cJSON *temperature_json = NULL;
-    cJSON *humid_json = NULL;
+    cJSON *humidity_json = NULL;
 
     char *string_json = NULL;
 
-   
-
     device_shadow_json_list = cJSON_CreateArray();
 
-    if (device_shadow_json_list == NULL) {
+    if (device_shadow_json_list == NULL)
+    {
         ESP_LOGE(TAG, "Json Init failed.");
         return -1;
     }
 
     device_shadow_json = cJSON_CreateObject();
 
-    if (device_shadow_json == NULL) {
+    if (device_shadow_json == NULL)
+    {
         ESP_LOGE(TAG, "Json add failed.");
         cJSON_Delete(device_shadow_json_list);
         return -1;
     }
 
-    struct timeval te;
-    gettimeofday(&te, NULL); // get current time
-    long long milliseconds = te.tv_sec * 1000LL + te.tv_usec / 1000;
+    // get current time
+    gettimeofday(&te, NULL);
+    milliseconds = te.tv_sec * 1000LL + te.tv_usec / 1000;
+
     timestamp_json = cJSON_CreateNumber(milliseconds);
 
-    if (timestamp_json == NULL) {
+    if (timestamp_json == NULL)
+    {
         ESP_LOGE(TAG, "Json add time stamp failed.");
         cJSON_Delete(device_shadow_json_list);
         return -1;
@@ -106,7 +135,8 @@ static int publish_device_shadow(bytebeam_client_t *bytebeam_client)
     sequence++;
     sequence_json = cJSON_CreateNumber(sequence);
 
-    if (sequence_json == NULL) {
+    if (sequence_json == NULL)
+    {
         ESP_LOGE(TAG, "Json add sequence id failed.");
         cJSON_Delete(device_shadow_json_list);
         return -1;
@@ -114,70 +144,65 @@ static int publish_device_shadow(bytebeam_client_t *bytebeam_client)
 
     cJSON_AddItemToObject(device_shadow_json, "sequence", sequence_json);
 
-    char temp_buff[50] = "Temperature and humidity Example";
-    int max_len = 200;
-
-    if(strlen(temp_buff) > max_len)
-    {
-        ESP_LOGE(TAG, "device status string size exceeded max length of buffer");
-    }
-
-    device_status_json = cJSON_CreateString(temp_buff);
-
-    if (device_status_json == NULL) {
-        ESP_LOGE(TAG, "Json add device status failed.");
-        cJSON_Delete(device_shadow_json_list);
-        return -1;
-    }
-
-    cJSON_AddItemToObject(device_shadow_json, "Status", device_status_json);
-
-    //get tempearture and humidity samples
-    getSHTValues();
+    // get tempearture and humidity samples
+    get_sht_values();
 
     temperature_json = cJSON_CreateNumber(temperature);
 
-    if (temperature_json == NULL) {
-        ESP_LOGE(TAG, "Json add time stamp failed.");
+    if (temperature_json == NULL)
+    {
+        ESP_LOGE(TAG, "Json add temperature failed.");
         cJSON_Delete(device_shadow_json_list);
         return -1;
     }
     
     cJSON_AddItemToObject(device_shadow_json, "temperature", temperature_json);
-
     
-    humid_json = cJSON_CreateNumber(humidity);
+    humidity_json = cJSON_CreateNumber(humidity);
 
-    if (humid_json == NULL) {
-        ESP_LOGE(TAG, "Json add time stamp failed.");
+    if (humidity_json == NULL)
+    {
+        ESP_LOGE(TAG, "Json add humidity failed.");
         cJSON_Delete(device_shadow_json_list);
         return -1;
     }
     
-    cJSON_AddItemToObject(device_shadow_json, "humidity", humid_json);
+    cJSON_AddItemToObject(device_shadow_json, "humidity", humidity_json);
 
     cJSON_AddItemToArray(device_shadow_json_list, device_shadow_json);
 
     string_json = cJSON_Print(device_shadow_json_list);
+
+    if(string_json == NULL)
+    {
+        ESP_LOGE(TAG, "Json string print failed.");
+        cJSON_Delete(device_shadow_json_list);
+        return -1;
+    }
+
     ESP_LOGI(TAG, "\nStatus to send:\n%s\n", string_json);
 
-    bytebeam_publish_to_stream(bytebeam_client, stream_name, string_json);
+    // publish the json to sht stream
+    int ret_val = bytebeam_publish_to_stream(bytebeam_client, sht_stream, string_json);
 
     cJSON_Delete(device_shadow_json_list);
-    free(string_json);
+    cJSON_free(string_json);
 
-    return 0;
+    return ret_val;
 }
 
 static void app_start(bytebeam_client_t *bytebeam_client)
 {
     int ret_val = 0;
 
-    while (1) {
-        ret_val = publish_device_shadow(bytebeam_client);
+    while (1)
+    {   
+        // publish sht values
+        ret_val = publish_sht_values(bytebeam_client);
 
-        if (ret_val != 0) {
-            ESP_LOGE(TAG, "Publish to device shadow failed");
+        if (ret_val != 0)
+        {
+            ESP_LOGE(TAG, "Failed to publish sht values.");
         }
 
         vTaskDelay(config_publish_period / portTICK_PERIOD_MS);
@@ -214,7 +239,8 @@ static void sync_time_from_ntp(void)
     initialize_sntp();
 
     // wait for time to be set
-    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count)
+    {
         ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
@@ -246,11 +272,20 @@ void app_main(void)
 
     ESP_ERROR_CHECK(example_connect());
 
+    // sync time from the ntp
     sync_time_from_ntp();
-    sht31_init();
+
+    // initialize the sht module
+    sht_init();
     
+    // initialize the bytebeam client
     bytebeam_init(&bytebeam_client);
+
+    // start the bytebeam client
     bytebeam_start(&bytebeam_client);
 
+    //
+    // start the main application
+    //
     app_start(&bytebeam_client);
 }
