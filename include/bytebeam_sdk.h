9d931b4b3492c9cf428f3794e64724e44af334aa
mayankbytebeam <mayanknarang@bytebeam.io>
2023-06-06 20:47:24 +0530

    bytebeam streams module abstracted

--- include/bytebeam_sdk.h
+++ include/bytebeam_sdk.h
@@ -9,25 +9,13 @@
 #include "bytebeam_client.h"
 #include "bytebeam_log.h"
 #include "bytebeam_actions.h"
+#include "bytebeam_streams.h"
 
 /*This macro is used to specify the maximum length of bytebeam OTA url string*/
 #define BYTEBAM_OTA_URL_STR_LEN 200
 
 /*This macro is used to specify the maximum length of the bytebeam OTA error string*/
 #define BYTEBEAM_OTA_ERROR_STR_LEN 200
-
-/**
- * @brief Publish message to particualar stream
- *
- * @param[in] bytebeam_client     bytebeam client handle
- * @param[in] stream_name         name of the target stream
- * @param[in] payload             message to publish
- * 
- * @return
- *      BB_SUCCESS : Message publish successful
- *      BB_FAILURE : Message publish failed
- */
-bytebeam_err_t bytebeam_publish_to_stream(bytebeam_client_t *bytebeam_client, char *stream_name, char *payload);
 
 /**
  * @brief Download and update Firmware image 
---
+++ include/bytebeam_streams.h
@@ -0,0 +1,19 @@
+#ifndef BYTEBEAM_STREAMS_H
+#define BYTEBEAM_STREAMS_H
+
+#include "bytebeam_client.h"
+
+/**
+ * @brief Publish message to particualar stream
+ *
+ * @param[in] bytebeam_client     bytebeam client handle
+ * @param[in] stream_name         name of the target stream
+ * @param[in] payload             message to publish
+ * 
+ * @return
+ *      BB_SUCCESS : Message publish successful
+ *      BB_FAILURE : Message publish failed
+ */
+bytebeam_err_t bytebeam_publish_to_stream(bytebeam_client_t *bytebeam_client, char *stream_name, char *payload);
+
+#endif /* BYTEBEAM_STREAMS_H */
